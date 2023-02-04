3671952d680f72c1c8712a3bd45b62a3f289a673
Brad Rhodes <1196817+bradrhodes@users.noreply.github.com>
2023-02-04 16:26:28 -0600

    Stickman ragdoll working but needs physics applied to it

---
+++ Assets/Game/Presentation/EnemyRagdoll.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.U2D.IK;
+
+public class EnemyRagdoll : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private List<Collider2D> colliders;
+    [SerializeField] private List<HingeJoint2D> joints;
+    [SerializeField] private List<Rigidbody2D> rigidBodies;
+    [SerializeField] private List<LimbSolver2D> limbSolvers;
+
+    private void Start()
+    {
+        RagdollOff();
+    }
+
+    public void RagdollOn() => ToggleRagdoll(true);
+    public void RagdollOff() => ToggleRagdoll(false);
+    private void ToggleRagdoll(bool ragdollOn)
+    {
+        animator.enabled = !ragdollOn;
+
+        foreach (var col in colliders)
+            col.enabled = ragdollOn;
+
+        foreach (var joint in joints)
+            joint.enabled = ragdollOn;
+
+        foreach (var body in rigidBodies)
+            body.simulated = ragdollOn;
+
+        foreach (var limbSolver in limbSolvers)
+        {
+            // Can use this instead to have more control
+            limbSolver.weight = ragdollOn ? 0 : 1;
+            // limbSolver.enabled = !ragdollOn;
+        }
+    }
+}
