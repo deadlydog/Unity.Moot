79db06960418d9d8c0837366ed059c08db9679a2
Andrew Kuipers <deimors@gmail.com>
2023-02-04 10:53:55 -0600

    Changed SampleScene to AndrewScene, starting AngularSpringJoint

---
+++ Assets/Game/Presentation/AngularSpringJoint.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Game.Presentation
+{
+	public class AngularSpringJoint : MonoBehaviour
+	{
+		private Rigidbody2D _rigidbody2d;
+		private float _originalRotation;
+
+		void Awake()
+		{
+			_rigidbody2d = GetComponent<Rigidbody2D>();
+			_originalRotation = _rigidbody2d.rotation;
+		}
+
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		void FixedUpdate()
+		{
+			var rotationDiff = _rigidbody2d.rotation - _originalRotation;
+
+			Debug.Log(rotationDiff);
+		}
+	}
+}
