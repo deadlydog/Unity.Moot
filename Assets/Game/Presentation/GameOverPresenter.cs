32fe4236b41a295328f3d842a44ff75b96c969c6
Andrew Kuipers <deimors@gmail.com>
2023-02-05 12:03:48 -0600

    Simple game over

---
+++ Assets/Game/Presentation/GameOverPresenter.cs
@@ -0,0 +1,34 @@
+using Assets.Game.Domain;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Game.Presentation
+{
+	public class GameOverPresenter : MonoBehaviour
+	{
+		private TextMeshProUGUI _text;
+
+		[Inject]
+		public IRootlingsEvents RootlingsEvents { private get; set; }
+
+		void Awake()
+		{
+			_text = GetComponent<TextMeshProUGUI>();
+		}
+
+		void Start()
+		{
+			SetGameOverVisibility(false);
+
+			RootlingsEvents
+				.OfType<RootlingsEvent, RootlingsEvent.AllStolen>()
+				.Subscribe(_ => SetGameOverVisibility(true))
+				.AddTo(this);
+		}
+
+		private void SetGameOverVisibility(bool visible)
+			=> _text.enabled = visible;
+	}
+}
